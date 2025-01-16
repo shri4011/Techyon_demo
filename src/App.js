f72bb236196877928f6ed5b9512b5f8511311a0b
rohit jasudkar <rohit.jasudkar@sumasoft.net>
2025-01-16 19:03:06 +0530

    Folder structure created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import Layout from "./layout/Layout";
+import Dashboard from "./pages/dashboard/Dashborad";
+
+function App() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+}
+
+export default App;
